d823c98362f157091a0744d36d46526cbf1a8ea6
px7066 <7066450@qq.com>
2019-10-22 16:49:47 +0800

    初始化包

---
+++ middle-mongo/src/main/java/com/github/middlemongo/MiddleMongoApplication.java
@@ -0,0 +1,13 @@
+package com.github.middlemongo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>启动类</p>
+ * @author <a href="7066450@qq.com">panxi</a>
+ * @version $Id$
+ * @since 1.0
+ */
+@SpringBootApplication
+public class MiddleMongoApplication {
+}
