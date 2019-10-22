90eab5a5eabb041d03163d742c6418baacb14254
px7066 <7066450@qq.com>
2019-10-22 16:34:45 +0800

    Initial commit

---
+++ src/main/java/com/github/middledemo/MiddleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.middledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddleDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/middledemo/MiddleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.middledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
